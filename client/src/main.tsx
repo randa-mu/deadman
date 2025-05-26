c981736502f8f6cf9328e59996bdb1608e92b161
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-05-26 15:44:58 +0200

    added toHex polyfill for chrome

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,26 @@ import {BrowserRouter} from "react-router-dom"
 import {App} from "@/App.tsx"
 import "./index.css"
 
+// polyfill for `Uint8Array.toHex()` because fuck chrome
+declare global {
+    interface Uint8Array {
+        /**
+         * Convert this byte array to a lowercase hex string
+         */
+        toHex(): string
+    }
+}
+
+if (!Uint8Array.prototype.toHex) {
+    Uint8Array.prototype.toHex = function(): string {
+        let hex = ""
+        for (const byte of this) {
+            hex += byte.toString(16).padStart(2, "0")
+        }
+        return hex
+    }
+}
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
